3f50c7bef7831b85df671153cb8010758b813560
viveking <sunilmondal4you@gmail.com>
2017-06-08 19:25:40 +0530

    commit testing according to with 550......

--- src/sunil/sample1.js
+++ src/sunil/sample1.js
@@ -13,27 +13,27 @@ var printer = new PdfPrinter(fonts);
 
 var dd = {
 
-    pageSize: { width: 700, height: 500 },
+    pageSize: { width: 550, height: 500 },
     content: [
         {
             columns: [
                 {
-                    width: 250,
+                    width: 200,
                     text: 'PAY WITH FREEPAY...EARN CASHBACK!!',
                     style: 'subheader'
                 },
                 {
-                    width: 150,
+                    width: 75,
                     text: 'TAX INVOICE ',
                     style: 'subheader',
                 },
                 {
-                    width: 175,
+                    width: 150,
                     text: 'FREEPAY MORE...EARN MORE!! ',
                     style: 'subheader'
                 },
                 {
-                    width: 100,
+                    width: 50,
                     text: 'ORIGINAL ',
                     style: 'subheader'
                 }
@@ -72,14 +72,14 @@ var dd = {
                 headerRows: 1,
                 body: [
                     [
-                        {text: 'PRODUCT NAME', bold: true},
-                        {text: 'M.R.P', bold: true},
-                        {text: 'CASS/PCS', bold: true},
-                        {text: 'RATE', bold: true},
-                        {text: 'SCHEME', bold: true},
-                        {text: 'VAT %', bold: true,},
-                        {text: 'VAT AMT.', bold: true},
-                        {text: 'NET AMT.', bold: true},
+                        {text: 'PRODUCT NAME', bold: true,border: [true, false, false, true]},
+                        {text: 'M.R.P', bold: true,border: [true, false, false, true]},
+                        {text: 'CASS/PCS', bold: true,border: [true, false, false, true]},
+                        {text: 'RATE', bold: true,border: [true, false, false, true]},
+                        {text: 'SCHEME', bold: true,border: [true, false, false, true]},
+                        {text: 'VAT %', bold: true,border: [true, false, false, true]},
+                        {text: 'VAT AMT.', bold: true,border: [true, false, false, true]},
+                        {text: 'NET AMT.', bold: true,border: [true, false, true, true]},
                     ],
                     [
                         {text: 'Tata Tea',border: [true, false, false, false]},
@@ -122,35 +122,35 @@ var dd = {
                         {text: '546.25',border: [true, false, true, false]},
                     ],
                     [
-                        {text: 'Tata Tea\n\n',border: [true, false, false, true]},
-                        {text: '100',border: [true, false, false, true]},
-                        {text: '5',border: [true, false, false, true]},
-                        {text: '100',border: [true, false, false, true]},
-                        {text: '5',border: [true, false, false, true]},
-                        {text: '15',border: [true, false, false, true]},
-                        {text: '71.25',border: [true, false, false, true]},
-                        {text: '546.25',border: [true, false, true, true]},
+                        {text: 'Tata Tea\n\n',border: [true, false, false, false]},
+                        {text: '100',border: [true, false, false, false]},
+                        {text: '5',border: [true, false, false, false]},
+                        {text: '100',border: [true, false, false, false]},
+                        {text: '5',border: [true, false, false, false]},
+                        {text: '15',border: [true, false, false, false]},
+                        {text: '71.25',border: [true, false, false, false]},
+                        {text: '546.25',border: [true, false, true, false]},
                     ]
                 ]
             },
 
         },
         {
-            style: 'tableExample2',
+            style: 'tableExample3',
             table: {
-                widths: [ '*', 50,50,50,50,50,50, 50],
-                height: [150],
+                widths: [ 150, '*','*','*','*','*','*', '*','*'],
                 headerRows: 1,
                 body: [
                     [
-                        {text: 'PRODUCT NAME', bold: true},
-                        {text: 'M.R.P', bold: true},
-                        {text: 'CASS/PCS', bold: true},
-                        {text: 'RATE', bold: true},
-                        {text: 'SCHEME', bold: true},
-                        {text: 'VAT %', bold: true,},
-                        {text: 'VAT AMT.', bold: true},
-                        {text: 'NET AMT.', bold: true},
+                        {text: 'For Unnati Trading Co.', bold: true},
+                        {text: 'Sale Ret Amt', bold: true},
+                        {text: 'Cash Disc', bold: true},
+                        {text: 'CR Adj Amt', bold: true},
+                        {text: 'DB Adj Amt', bold: true},
+                        {text: 'QPS Amt', bold: true,},
+                        {text: 'Scheme Amt.', bold: true},
+                        {text: 'Total Amt.', bold: true},
+                        {text: 'Net Amt.', bold: true},
                     ],
                     [
                         {text: 'Tata Tea',border: [true, false, false, true]},
@@ -160,6 +160,7 @@ var dd = {
                         {text: '5',border: [true, false, false, true]},
                         {text: '15',border: [true, false, false, true]},
                         {text: '71.25',border: [true, false, false, true]},
+                        {text: '546.25',border: [true, false, true, true]},
                         {text: '546.25',border: [true, false, true, true]},
                     ]
                 ]
@@ -175,7 +176,7 @@ var dd = {
             bold: true
         },
         subheader: {
-            fontSize: 10,
+            fontSize: 8,
             bold: true
         },
         bigger: {
@@ -183,13 +184,20 @@ var dd = {
             italics: true,
         },
         tableExample1: {
-            margin: [0, 5, 0, 0],
+            margin: [0, 0, 0, 0],
             alignment: 'center',
         },
         tableExample2: {
             margin: [0, 0, 0, 0],
             alignment: 'center',
             fontSize: 10,
+
+
+        },
+        tableExample3: {
+            margin: [0, 0, 0, 0],
+            alignment: 'center',
+            fontSize: 8,
 
 
         }
@@ -199,6 +207,7 @@ var dd = {
         columnGap: 0,
     }
 };
+
 
 
 var pdfDoc = printer.createPdfKitDocument(dd);
