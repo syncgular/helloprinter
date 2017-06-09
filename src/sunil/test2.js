4e46b0e91c34100726a315ce0486a85789d4b69d
viveking <sunilmondal4you@gmail.com>
2017-06-09 19:14:09 +0530

    commit testing according to with 550......

--- src/sunil/test2.js
+++ src/sunil/test2.js
@@ -1,3 +1,12 @@
 /**
  * Created by abc on 08/06/2017.
  */
+
+var BrowserWindow  = require('electron').remote;
+var PDFWindow = require('electron-pdf-window');
+
+var win = new BrowserWindow({ width: 800, height: 600 });
+
+PDFWindow.addSupport(win);
+
+win3.loadURL('file://src/sunil/test.html');
