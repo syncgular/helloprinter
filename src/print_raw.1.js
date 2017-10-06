d6c48a2b6295c23f468f82c0c0347a28a296a11d
santosh <santosh.ece06@gmail.com>
2017-10-06 13:41:17 +0530

    ttt

---
+++ src/print_raw.1.js
@@ -0,0 +1,23 @@
+﻿var printer = require("printer");
+
+
+String.prototype.toBytes = function() {
+    var arr = []
+    for (var i = 0; i < this.length; i++) {
+        arr.push(this[i].charCodeAt(0))
+    }
+    return arr;
+}
+
+var printData = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890\n".toBytes().concat([0x1b, 0x4d, 0x01]);
+console.log(printData);
+
+printer.printDirect({
+    data: new Buffer(printData),
+    printer: 'EPSON_LX_300_',
+    type: 'RAW',
+    success: function(jobID) {
+        console.log("sent to printer with ID: " + jobID);
+    },
+    error: function(err) { console.log(err); }
+});
